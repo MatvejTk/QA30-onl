8788ea9b713b46d2b5ddf76c3e4cb5ab2125be6d
Matvej <matvej@MacBook-Air-Elizaveta.local>
2025-03-09 17:31:19 +0300

    new

--- src/lesson_3/hometask/task2_cycles/Task1.java
+++ src/lesson_3/hometask/task2_cycles/Task1.java
@@ -4,7 +4,6 @@ public class Task1 {
     public static void main(String[] args) {
         allEvenNumbers();
     }
-
     public static void allEvenNumbers() {
         for (int numb = 1; numb < 100; numb++) {
             if (numb % 2 == 1) {
--- src/lesson_3/hometask/task2_cycles/Task3.java
+++ src/lesson_3/hometask/task2_cycles/Task3.java
@@ -1,4 +1,22 @@
 package lesson_3.hometask.task2_cycles;
+import java.util.Scanner;
+
+import static java.lang.Long.sum;
 
 public class Task3 {
+    public static void main(String[] args) {
+        countNumbers();
+    }
+    public static void countNumbers() {
+        Scanner input = new Scanner (System.in);
+        System.out.println("Please insert any positive integer number: ");
+        int number = input.nextInt();
+        for (int a = 1; a < number; a++);
+        if (number > 0) {
+            System.out.println("This is sum of numbers from 1 to your inserted number: " + number);
+        }
+        else {
+            System.out.println("You inserted incorrect number. Should be 1+");
+        }
+    }
 }
