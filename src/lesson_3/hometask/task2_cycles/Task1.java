5c6f441c1ca9e4b6b6484757a230d6e00efd9ef7
Matvej <matvej@MacBook-Air-Elizaveta.local>
2025-03-09 16:47:09 +0300

    changes

--- src/lesson_3/hometask/task2_cycles/Task1.java
+++ src/lesson_3/hometask/task2_cycles/Task1.java
@@ -4,6 +4,12 @@ public class Task1 {
     public static void main(String[] args) {
         allEvenNumbers();
     }
+
     public static void allEvenNumbers() {
+        for (int numb = 1; numb < 100; numb++) {
+            if (numb % 2 == 1) {
+                System.out.print(numb + " ");
+            }
+        }
     }
 }
