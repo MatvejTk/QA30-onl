ad9d9273d8334f9a2b3d7017ad4d3cfb7adad4ee
Matvej <matvej@MacBook-Air-Elizaveta.local>
2025-03-09 13:21:44 +0300

    updates

--- src/lesson_3/hometask/task2_cycles/Task5.java
+++ src/lesson_3/hometask/task2_cycles/Task5.java
@@ -1,30 +1,16 @@
 package lesson_3.hometask.task2_cycles;
 
 public class Task5 {
-    public static void main(String[] args) {
-        increment(10);
-    }
-    public static void increment(int n) {
-        int numbers = 0;
-        while (numbers < 100) {
-            System.out.println(numbers);
-            numbers += 7;
-        }
-    }
+
+public static void main(String[] args) {
+    printSubsequence(10);
 }
-/*
-    public static void main(String[] args) {
-        printSubsequence(10);
-    }
-    static void printSubsequence(int n) {
-        int f1 = 0, f2 = -5;
+static void printSubsequence(int n) {
+    int f1 = 0;
+
+    for (int i = 0; i < n; i++) {
         System.out.print(f1 + " ");
-        for (int i = 1; i < n; i++) {
-            System.out.print(f2 + " ");
-            int next = f1 + f2;
-            f1 = f2;
-            f2 = next;
-        }
+        f1 -=5;
     }
 }
- */
+}
--- src/lesson_3/hometask/task2_cycles/Task6.java
+++ src/lesson_3/hometask/task2_cycles/Task6.java
@@ -1,15 +1,2 @@
 public class Task6 {
-    public static void main(String[] args) {
-        printSubsequence(10);
-    }
-    static void printSubsequence(int n) {
-        int f1 = 0, f2 = -5;
-
-        System.out.print(f1 + " ");
-
-        for (int i = 0; i < n; i++) {
-            System.out.print(f2 + " ");
-           f2 -=5;
-        }
-    }
 }
